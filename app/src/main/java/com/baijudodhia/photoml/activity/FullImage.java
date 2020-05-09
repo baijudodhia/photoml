84fc8351ab3dd7145ead55783b2dfd8ef2432297
baijudodhia <51258945+baijudodhia@users.noreply.github.com>
2020-05-10 03:52:30 +0530

    Added Firebase ML-Kit Label Detector on button click

--- app/src/main/java/com/baijudodhia/photoml/activity/FullImage.java
+++ app/src/main/java/com/baijudodhia/photoml/activity/FullImage.java
@@ -1,15 +1,29 @@
 package com.baijudodhia.photoml.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.baijudodhia.photoml.R;
 import com.bumptech.glide.Glide;
 import com.github.chrisbanes.photoview.PhotoView;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FullImage extends AppCompatActivity {
     String imagePath;
@@ -43,6 +57,7 @@ public class FullImage extends AppCompatActivity {
         int id = item.getItemId();
         if (id == R.id.menu_actionbar_label) {
             //Call method to perform Label Detection
+            LabelDetection();
         }
         if (id == R.id.menu_actionbar_ocr) {
             //Call method to perform OCR
@@ -54,5 +69,68 @@ public class FullImage extends AppCompatActivity {
             //Call method to perform Barcode Detection
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //Show AlertDialog containing result of Firebase ML-Kit detection
+    public void showmessage(String title, StringBuilder builder) {
+        final AlertDialog.Builder alertbuilder = new AlertDialog.Builder(this);
+        alertbuilder.setCancelable(true);
+        alertbuilder.setTitle(title);
+        if (builder.length() != 0) {
+            alertbuilder.setMessage(builder);
+        } else {
+            alertbuilder.setMessage(title + " not detected!");
+        }
+        alertbuilder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alertbuilder.show();
+    }
+
+    //Label Detection
+    public void LabelDetection() {
+        final StringBuilder builder = new StringBuilder();
+        final ArrayList<String> labeltext = new ArrayList<String>();
+        BitmapDrawable drawable = (BitmapDrawable) fullImageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance().getOnDeviceImageLabeler();
+        labeler.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                        for (FirebaseVisionImageLabel label : labels) {
+                            String text = label.getText();
+                            float confidence = label.getConfidence();
+                            //Only add labels with confidence greater than 0.5
+                            if (confidence > 0) {
+                                labeltext.add(text + " [" + String.format("%.2f", confidence) + "]");
+                            }
+                        }
+                        if (labels.size() != 0) {
+                            builder.append("Labels Detected\n");
+                        } else {
+                            builder.append("Labels Not Detected!");
+                        }
+                        int i = 1;
+                        for (String label : labeltext) {
+                            builder.append(i + ". " + label + "\n");
+                            i++;
+                        }
+                        builder.append("\n");
+                        showmessage("Label Detecion", builder);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        StringBuilder builder = new StringBuilder();
+                        builder.append("Apologies :(\nLabel Detector encountered a problem!\n");
+                        showmessage("Label Detecion", builder);
+                    }
+                });
     }
 }
